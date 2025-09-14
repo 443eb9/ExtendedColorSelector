13a99bdba5aad261ff899fceae7132570f51e862
443eb9#C <443eb9@gmail.com>
2025-09-14 18:01:55 +0800

    feat: Release 0.4.0

--- create_artifacts.ps1
+++ create_artifacts.ps1
@@ -4,4 +4,4 @@
 Copy-Item .\target\release\axes_limits_compute.exe .\artifacts
 Copy-Item .\target\x86_64-unknown-linux-gnu\release\axes_limits_compute .\artifacts
 
-Compress-Archive -Path .\extended_color_selector, .\extended_color_selector.desktop -DestinationPath .\artifacts\ExtendedColorSelector.zip 
+Compress-Archive -Path .\extended_color_selector, .\extended_color_selector.desktop -DestinationPath .\artifacts\ExtendedColorSelector.zip -Force
