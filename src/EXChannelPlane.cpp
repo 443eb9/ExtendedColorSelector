bf48b52c2ee472fecc235c2f9b9a1c3d96d1b7d5
443eb9#C <443eb9@gmail.com>
2025-10-09 08:27:10 +0800

    fix: Ring size not update on resize.

--- src/EXChannelPlane.cpp
+++ src/EXChannelPlane.cpp
@@ -136,6 +136,7 @@ void EXChannelPlane::settingsChanged()
 void EXChannelPlane::resizeEvent(QResizeEvent *event)
 {
     QWidget::resizeEvent(event);
+    settingsChanged();
     updateImage();
 }
 
